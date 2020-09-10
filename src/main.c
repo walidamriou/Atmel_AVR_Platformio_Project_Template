508c4fa01a9e31014d6c50a1ff942dff3887b0ad
Walid Amriou <walidamriou@gmail.com>
2020-09-10 21:43:09 +0100

    update

---
+++ src/main.c
@@ -0,0 +1,20 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+    
+    // make the LED pin an output for PORTB5
+    DDRB = 1 << 5;
+
+    while (1)
+    {
+        _delay_ms(500);
+
+
+        // toggle the LED
+        PORTB ^= 1 << 5;
+    }
+
+    return 0;
+}
